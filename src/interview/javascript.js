4aa93a57227d49295971181609ae99a9f7758cec
sluggishpj <sluggishpj@gmail.com>
2021-12-09 20:18:28 +0800

    feat: update

--- src/interview/javascript.js
+++ src/interview/javascript.js
@@ -1,10 +1,59 @@
-// 距上一次执行必须 超过 wait 毫秒后才会再执行
-export function debounce(fn, wait = 0) {
-  let lastTime = 0
+/**
+ * 距上一次 调用 超过 wait 毫秒后才会再执行
+ * @param {Function} fn
+ * @param {number} wait 等待ms
+ * @param {boolean} leading 首次是否立即执行
+ * @returns
+ */
+export function debounce(fn, wait = 0, leading = false) {
+  let timer = null
+
   return function afterDebounce(...args) {
-    if (Date.now() - lastTime < wait) {
+    if (!timer && leading) {
+      // 无需等待，且需要立即执行
+      fn.apply(this, args)
+      timer = setTimeout(() => {
+        timer = null
+      }, wait)
+    } else {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        fn.apply(this, args)
+        timer = null
+      }, wait)
+    }
+  }
+}
+
+// 一直调用，最多每隔 wait ms 执行1次
+export function throttle(fn, wait = 0, leading = true, trailing = true) {
+  let lastTime = 0
+  let lastTimer = null // 当 trailing 为true时，则最后1次需要调用
+
+  return function afterThrottle(...args) {
+    if (lastTime === 0) {
+      // 说明是首次
+      if (leading) {
+        lastTime = Date.now()
+        return fn.apply(this, args)
+      }
+    }
+
+    if (trailing) {
+      clearTimeout(lastTimer)
+    }
+
+    const timeGap = Date.now() - lastTime
+    if (timeGap < wait) {
+      // 还在时间间隔内
+      if (trailing) {
+        lastTimer = setTimeout(() => {
+          fn.apply(this, args)
+        }, wait - timeGap)
+      }
       return
     }
+
     lastTime = Date.now()
     return fn.apply(this, args)
   }
--- tests/unit/interview/javascript.spec.js
+++ tests/unit/interview/javascript.spec.js
@@ -1,43 +1,117 @@
 import { advanceBy, clear } from 'jest-date-mock'
-import { debounce } from '@/interview/javascript'
+import { debounce, throttle } from '@/interview/javascript'
+
+afterEach(() => {
+  clear()
+  jest.useRealTimers()
+})
 
 describe('debounce', () => {
-  it('规定时间内多次调用，只执行第1次', () => {
+  it('首次不执行', () => {
+    jest.useFakeTimers()
     let i = 0
-    function test() {
+    function test(cb) {
+      cb?.()
       i++
       return i
     }
 
-    const debounceTest = debounce(test, 200)
+    const callback1 = jest.fn()
 
-    expect(debounceTest()).toBe(1)
-    expect(debounceTest()).toBe(undefined)
-    expect(debounceTest()).toBe(undefined)
-    expect(debounceTest()).toBe(undefined)
+    const debounceTest = debounce(test, 200)
+    debounceTest(callback1) // 首次不执行
+    expect(callback1).not.toBeCalled()
+    expect(i).toBe(0)
+
+    const callback2 = jest.fn()
+    jest.advanceTimersByTime(150)
+    debounceTest(callback2) // 间隔内也不执行
+    expect(callback1).not.toBeCalled()
+    expect(callback2).not.toBeCalled()
+    expect(i).toBe(0)
+
+    jest.advanceTimersByTime(200) // 又过了200ms了，执行最近的一次执行
+    expect(callback1).not.toBeCalled()
+    expect(callback2).toHaveBeenCalledTimes(1)
     expect(i).toBe(1)
+
+    const callback3 = jest.fn()
+    debounceTest(callback3)
+    jest.advanceTimersByTime(200)
+    expect(callback1).not.toBeCalled()
+    expect(callback2).toHaveBeenCalledTimes(1)
+    expect(callback3).toHaveBeenCalledTimes(1)
+    expect(i).toBe(2)
   })
 
-  it('规定时间外多次调用，可以正常调用', () => {
+  it('首次执行', () => {
+    jest.useFakeTimers()
     let i = 0
-    function test() {
+    function test(cb) {
+      cb?.()
       i++
       return i
     }
 
-    const debounceTest = debounce(test, 200)
+    const callback1 = jest.fn()
 
-    expect(debounceTest()).toBe(1)
-
-    expect(debounceTest()).toBe(undefined)
-    expect(debounceTest()).toBe(undefined)
+    const debounceTest = debounce(test, 200, true)
+    debounceTest(callback1) // 首次执行
+    expect(callback1).toHaveBeenCalledTimes(1)
     expect(i).toBe(1)
 
-    advanceBy(200)
-    expect(debounceTest()).toBe(2)
-    expect(debounceTest()).toBe(undefined)
+    const callback2 = jest.fn()
+    jest.advanceTimersByTime(150)
+    debounceTest(callback2) // 间隔内不执行
+    expect(callback1).toHaveBeenCalledTimes(1)
+    expect(callback2).not.toBeCalled()
+    expect(i).toBe(1)
+
+    jest.advanceTimersByTime(200) // 又过了200ms了，执行最近的一次执行
+    expect(callback1).toHaveBeenCalledTimes(1)
+    expect(callback2).toHaveBeenCalledTimes(1)
     expect(i).toBe(2)
 
-    clear()
+    const callback3 = jest.fn()
+    const callback4 = jest.fn()
+    debounceTest(callback3) // 立即执行
+    debounceTest(callback4) // 间隔内不执行
+    expect(callback3).toHaveBeenCalledTimes(1)
+    expect(i).toBe(3)
+
+    jest.advanceTimersByTime(200) // 又过了200ms了，执行最近的一次执行
+    expect(callback4).toHaveBeenCalledTimes(1)
+    expect(i).toBe(4)
+  })
+})
+
+describe('throttle', () => {
+  it('首次执行+多个时末尾也执行', () => {
+    jest.useFakeTimers()
+
+    let i = 0
+    function test(cb) {
+      cb?.()
+      i++
+      return i
+    }
+
+    const callback = jest.fn()
+    const throttleTest = throttle(test, 200)
+    expect(throttleTest(callback)).toBe(1) // 首个执行
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    // 间隔内多次只执行到时间后只执行最后1次
+    const callback2 = jest.fn()
+    const callback3 = jest.fn()
+    expect(throttleTest(callback2)).toBe(undefined)
+    expect(throttleTest(callback3)).toBe(undefined)
+    expect(callback2).toHaveBeenCalledTimes(0)
+    expect(callback3).toHaveBeenCalledTimes(0)
+    expect(i).toBe(1)
+
+    jest.advanceTimersByTime(200)
+    expect(callback3).toHaveBeenCalledTimes(1)
+    expect(i).toBe(2)
   })
 })
